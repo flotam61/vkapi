48a1afb287c9784beb4fb515fe2faf5328344613
flotam61 <flotam7@gmail.com>
2021-10-01 16:26:06 +0300

    def version

--- main.py
+++ main.py
@@ -4,48 +4,68 @@ from tqdm import tqdm
 
 # 76119731
 
-vktoken = input("Введите токен vkapi ")
-yatoken = input("Введите токен Яндекс диска, куда хотите загрузить фотографии ")
-idvk = input("Введите id страницы в вконтакте ")
+def menu():
+    print("Программа сохраняет фотографии с разных API на яДиск. Выберите API")
+    global choise
+    choise = input("Возможные варианты: vk, instagram, ok. (Пока возможно только вк) ")
+    global yatoken
+    yatoken = input("Введите Token яДиска, куда загрузить фотографии ")
+    if choise == "vk" or choise == "VK":
+        print()
+        vkphoto()
+    else:
+        print("Неверно! Напишите <vk>, остальные функции появятся в сл. версии программы.")
+        return menu()
 
-url = "https://api.vk.com/method/photos.get"
-params = {"owner_id": idvk, "album_id": "profile", "photo_sizes": 1, "extended": 1, "count": 5, "access_token": vktoken, "v": "5.131"}
+def vkphoto():
+    print("Функция загружает на яДиск необходимое кол-во фото с аватарок vk данного ID")
+    print("И сохраняет список файлов в формате <Имя : размер> в файл <savejson.json>")
+    vktoken = input("Введите token VK ")
+    global idvk
+    idvk = input("Введите ID пользователя vk ")
+    countphotos = int(input("Сколько фотографий загрузить? "))
 
-res = requests.get(url, params=params).json()
-count = params["count"]
-nameid = params["owner_id"]
-yx = 0
-listphotos = {}
-savejson = []
+    url = "https://api.vk.com/method/photos.get"
+    params = {"owner_id": idvk, "album_id": "profile", "photo_sizes": 1, "extended": 1, "count": countphotos,
+              "access_token": vktoken, "v": "5.131"}
+    resvk = requests.get(url, params=params).json()
 
-while yx < count:
-    likes = str(res["response"]["items"][yx]["likes"]["count"]) + ".jpg"
-    listphotos[likes] = res["response"]["items"][yx]["sizes"][-1]["url"]
-    savejson.append({"file_name": likes, "size": res["response"]["items"][yx]["sizes"][-1]["type"]})
-    yx += 1
+    y = 0
+    global listphotos
+    listphotos = {}
+    savejson = []
 
-with open('savejson.json', 'w') as outfile:
-    json.dump(savejson, outfile)
+    while y < countphotos:
+        likes = str(resvk["response"]["items"][y]["likes"]["count"]) + ".jpg"
+        listphotos[likes] = resvk["response"]["items"][y]["sizes"][-1]["url"]
+        savejson.append({"file_name": likes, "size": resvk["response"]["items"][y]["sizes"][-1]["type"]})
+        y += 1
 
-headers = {
-    "Accept": "application/json",
-    "Authorization": "OAuth " + yatoken
-}
+    with open('savejson.json', 'w') as outfile:
+        json.dump(savejson, outfile)
+    upload_yadisk()
 
-url_patch = "https://cloud-api.yandex.net/v1/disk/resources/"
-params_patch = {
-    'path': "/vkapi/id" + nameid
-}
-r = requests.put(url=url_patch, params=params_patch, headers=headers)
-
-for nameurl in tqdm(listphotos):
-    params = {
-        'path':"/vkapi/id" + nameid + "/" + str(nameurl),
-        'url': listphotos[nameurl]
+def upload_yadisk():
+    print()
+    print("Загружаем фото на яДиск")
+    headers = {
+        "Accept": "application/json",
+        "Authorization": "OAuth " + yatoken
     }
-    url_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload/"
-    r = requests.post(url=url_upload, params=params, headers=headers)
-    res_upload = r.json()
-    print(res_upload)
+    url_create_folder = "https://cloud-api.yandex.net/v1/disk/resources/"
+    if choise == "vk" or choise == "VK":
+        params_create_folder = {
+            'path': "/vkapi/id" + idvk
+        }
+        res_folder = requests.put(url=url_create_folder, params=params_create_folder, headers=headers)
+        for name_url in tqdm(listphotos):
+            params = {
+                'path': "/vkapi/id" + idvk + "/" + str(name_url),
+                'url': listphotos[name_url]
+            }
+            url_upload = "https://cloud-api.yandex.net/v1/disk/resources/upload/"
+            r = requests.post(url=url_upload, params=params, headers=headers)
+            res_upload = r.json()
+            print(res_upload)
 
-
+menu()
